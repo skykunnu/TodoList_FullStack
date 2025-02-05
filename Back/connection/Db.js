78eed39ce1a1596463e56d460143250ad9b114ca
Shikhar Khandelwal <skykunnu@gmail.com>
2025-02-05 10:54:42 +0530

    minorUpdates

--- Back/connection/Db.js
+++ Back/connection/Db.js
@@ -4,5 +4,4 @@ import "dotenv/config";
 
 export async function connectToDB(){
     await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.dlqaa.mongodb.net/todolistfullstack?retryWrites=true&w=majority&appName=Cluster0`);
-    
 }
